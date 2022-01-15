7dcaf4b059e8dca8a3de68d36bb2949cdc42c878
dylanke <keson_xiao@qq.com>
2022-01-15 17:06:58 +0800

    重构react创建的实例

---
+++ src/TodoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+  state = {}
+  render () {
+    return (
+      <div>
+        <div>
+          <input />
+          <button>提交</button>
+        </div>
+        <ul>
+          <li>hello</li>
+        </ul>
+      </div>
+      )
+  }
+}
+
+export default TodoList
